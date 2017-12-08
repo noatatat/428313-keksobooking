b284de1a6992cf3c0a54fc3774d35af655f0a04f
noatatat <t.b.obuhov@gmail.com>
2017-12-08 23:39:38 +0300

    feature: добавляет функцию перетаскивания центрального пина по карте

--- js/map.js
+++ js/map.js
@@ -12,9 +12,37 @@
 
   var mapPinMain = window.mapBlock.querySelector('.map__pin--main');
   window.noticeForm = document.querySelector('.notice__form');
-  mapPinMain.addEventListener('mouseup', function () {
+
+  mapPinMain.addEventListener('click', activateMap);
+  function activateMap() {
     window.mapBlock.classList.remove('map--faded');
     window.noticeForm.classList.remove('notice__form--disabled');
     disableEachInArray(formFieldsets, false);
-  });
+    addDrugFeature();
+    mapPinMain.removeEventListener('mouseup', activateMap);
+  }
+
+  function addDrugFeature() {
+    mapPinMain.draggable = true;
+    mapPinMain.addEventListener('dragstart', onMainPinDragStart);
+  }
+
+  var shift = {};
+  function onMainPinDragStart(evt) {
+    shift = {
+      x: evt.clientX - mapPinMain.offsetLeft,
+      y: evt.clientY - mapPinMain.offsetTop
+    };
+    mapPinMain.addEventListener('dragend', onMainPinDragEnd);
+  }
+
+  function onMainPinDragEnd(endEvt) {
+    var coords = {
+      x: endEvt.clientX - shift.x,
+      y: endEvt.clientY - shift.y
+    };
+    mapPinMain.style.left = coords.x + 'px';
+    mapPinMain.style.top = coords.y + 'px';
+    mapPinMain.removeEventListener('dragend', onMainPinDragEnd);
+  }
 })();
