755765e77dd5e930d4f55d70c4598b5c7070bc0b
noatatat <t.b.obuhov@gmail.com>
2017-12-12 18:52:35 +0300

    feat: загружает объявления с сервера и расставляет метки по карте

--- js/pins.js
+++ js/pins.js
@@ -5,23 +5,29 @@
   window.mapBlock = document.querySelector('.map');
   var mapPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
-  var createMapPin = function (advertisement) {
-    var mapPin = mapPinTemplate.cloneNode(true);
-    var x = advertisement.location.x - window.PIN_WIDTH / 2;
-    var y = advertisement.location.y - window.PIN_HEIGHT;
-    mapPin.style = 'left: ' + x + 'px; top: ' + y + 'px;';
-    mapPin.querySelector('img').src = advertisement.author.avatar;
-    return mapPin;
-  };
+  function createPins(data) {
+    window.advertisements = data;
 
-  var fragmentPins = document.createDocumentFragment();
-  for (var j = 0; j < window.ADVERTICEMENT_NUMBER; j++) {
-    fragmentPins.appendChild(createMapPin(window.advertisements[j]));
+    var createMapPin = function (advertisement) {
+      var mapPin = mapPinTemplate.cloneNode(true);
+      var x = advertisement.location.x - window.PIN_WIDTH / 2;
+      var y = advertisement.location.y - window.PIN_HEIGHT;
+      mapPin.style = 'left: ' + x + 'px; top: ' + y + 'px;';
+      mapPin.querySelector('img').src = advertisement.author.avatar;
+      return mapPin;
+    };
+
+    window.fragmentPins = document.createDocumentFragment();
+    for (var i = 0; i < window.ADVERTICEMENT_NUMBER; i++) {
+      window.fragmentPins.appendChild(createMapPin(window.advertisements[i]));
+    }
   }
+
+  window.backend.load(createPins, window.utils.showErrorMessage);
 
   window.showPins = showPins;
   function showPins() {
-    mapPinsBlock.appendChild(fragmentPins);
+    mapPinsBlock.appendChild(window.fragmentPins);
     var pins = window.mapBlock.querySelectorAll('.map__pin:not(.map__pin--main)');
     pins.forEach(function (pin) {
       pin.addEventListener('click', function (evt) {
