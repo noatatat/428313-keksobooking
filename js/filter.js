318196d02be1127eedf27f78e78c172113a69076
noatatat <t.b.obuhov@gmail.com>
2017-12-16 16:59:36 +0300

    feat: создает разовый фильтр при смене папаметров фильтрации и выводит рехультаты

--- js/filter.js
+++ js/filter.js
@@ -2,7 +2,20 @@
 
 (function () {
   var filterContainer = document.querySelector('.map__filters');
-  filterContainer.addEventListener('change', function (evt) {
-    console.log(evt.target);
-  });
+  filterContainer.addEventListener('change', onFilterChange);
+
+  function onFilterChange(evt) {
+    //var selectedIndex = evt.target.options.selectedIndex;
+    //console.log(evt.target.options[selectedIndex].value);
+    //console.log(window.advertisements);
+
+    window.filteredAdvertisement = window.advertisements
+        .filter(setTypeChange);
+    window.showPins(window.filteredAdvertisement);
+    console.log(window.filteredAdvertisement);
+  }
+
+  function setTypeChange(advertisement) {
+    return advertisement.offer.type === 'flat';
+  }
 })();
