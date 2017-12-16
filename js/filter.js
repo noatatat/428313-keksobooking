8de770f6b486b8998cface14294c4cd9b6088ef6
noatatat <t.b.obuhov@gmail.com>
2017-12-16 19:19:19 +0300

    feat: реализует фильты по стоимости, количеству комнат и гостей

--- js/filter.js
+++ js/filter.js
@@ -1,23 +1,60 @@
 'use strict';
 
 (function () {
+  var PRICE_EDGE = {
+    low: 10000,
+    middle: 50000
+  };
   window.filterPins = filterPins;
   function filterPins() {
-    var filterContainer = document.querySelector('.map__filters');
-    var housingType = filterContainer.querySelector('#housing-type');
+    var filter = document.querySelector('.map__filters');
+    var housingType = filter.querySelector('#housing-type');
+    var housingPrice = filter.querySelector('#housing-price');
+    var housingRooms = filter.querySelector('#housing-rooms');
+    var housingGuests = filter.querySelector('#housing-guests');
+
     window.filteredAdvertisements = window.advertisements.slice();
 
-    filterContainer.addEventListener('change', onFilterChange);
+    filter.addEventListener('change', onFilterChange);
 
     function onFilterChange() {
       window.removeMapCard();
       window.filteredAdvertisements = window.advertisements
-          .filter(setTypeChange);
+          .filter(setTypeChange)
+          .filter(setPriceChange)
+          .filter(setRoomsChange)
+          .filter(setGuestChange);
       window.showPins(window.filteredAdvertisements);
+
     }
 
     function setTypeChange(advertisement) {
-      return (!housingType.options.selectedIndex) || (advertisement.offer.type === getSelectedValue(housingType));
+      var type = advertisement.offer.type;
+      return (!housingType.options.selectedIndex) || (type === getSelectedValue(housingType));
+    }
+
+    function setPriceChange(advertisement) {
+      var price = advertisement.offer.price;
+      switch (getSelectedValue(housingPrice)) {
+        case 'low' :
+          return price < PRICE_EDGE.low;
+        case 'middle' :
+          return (price >= PRICE_EDGE.low) && (price < PRICE_EDGE.middle);
+        case 'high' :
+          return price >= PRICE_EDGE.middle;
+        default :
+          return true;
+      }
+    }
+
+    function setRoomsChange(advertisement) {
+      var rooms = advertisement.offer.rooms.toString();
+      return (!housingRooms.options.selectedIndex) || (rooms === getSelectedValue(housingRooms));
+    }
+
+    function setGuestChange(advertisement) {
+      var guests = advertisement.offer.guests.toString();
+      return (!housingGuests.options.selectedIndex) || (guests === getSelectedValue(housingGuests));
     }
 
     function getSelectedValue(selectName) {
