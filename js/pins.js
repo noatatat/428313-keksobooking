00f67051c91e9c80016cf626b658f58b840823ba
Vladimir Vorobjev <C:\Users\vorobjev\AppData\Roaming\The Bat!>
2017-12-19 13:27:52 +0300

    style: частично переписал функцию перебора меток через метод прототипа

--- js/pins.js
+++ js/pins.js
@@ -35,7 +35,7 @@
     removePins();
     mapPinsBlock.appendChild(createPinFragment(advertisements));
     var pins = window.mapBlock.querySelectorAll('.map__pin:not(.map__pin--main)');
-    pins.forEach(function (pin) {
+    [].forEach.call(pins, function (pin) {
       pin.addEventListener('click', function (evt) {
         window.showCard(evt, pins);
       });
